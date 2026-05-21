c566acf70a664e3e4bac33fdae0841132f27bc5d
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-21 14:50:22 +0530

    Booking Model generated

---
+++ src/models/bookings.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Bookings extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Bookings.init({
+    flightId: {
+      type: DataTypes.INTEGER, 
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER, 
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+      values: ["In Process", "Booked", "Cancelled"],
+      defaultValue: "In Process"
+    }
+  }, {
+    sequelize,
+    modelName: 'Bookings',
+  });
+  return Bookings;
+};
